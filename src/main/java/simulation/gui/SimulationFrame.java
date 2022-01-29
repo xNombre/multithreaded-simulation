d9f63b63c44ff25447033d6eb2164241e8b78d2f
Patryk Lason <patryk.lason12@gmail.com>
2022-01-29 16:19:13 +0100

    Add vehicles to simulation, change panel background

--- src/main/java/simulation/gui/SimulationFrame.java
+++ src/main/java/simulation/gui/SimulationFrame.java
@@ -1,5 +1,7 @@
 package simulation.gui;
 
+import simulation.simulation.Ambulance;
+import simulation.simulation.Vehicle;
 import simulation.simulation.Witness;
 
 import javax.swing.*;
@@ -9,6 +11,7 @@ import java.util.ArrayList;
 public class SimulationFrame extends JFrame {
 
     static ArrayList<Witness> witnesses = new ArrayList<>();
+    static ArrayList<Vehicle> vehicles = new ArrayList<>();
 
     private static class SimulationPanel extends JPanel {
         @Override
@@ -17,6 +20,10 @@ public class SimulationFrame extends JFrame {
 
             for(Witness witness : witnesses){
                 witness.paint(g);
+            }
+
+            for(Vehicle vehicle : vehicles) {
+                vehicle.paint(g);
             }
         }
     }
@@ -28,11 +35,17 @@ public class SimulationFrame extends JFrame {
         this.initializeProperties();
 
         SimulationPanel panel = new SimulationPanel();
+        panel.setBackground(Color.gray);
         this.add(panel);
 
+        // Change this values later
         Witness.setBorder(600, 600);
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             witnesses.add(new Witness(panel));
+        }
+
+        for (int i = 0; i < 1; i++) {
+            vehicles.add(new Ambulance(100,100, panel));
         }
 
     }
