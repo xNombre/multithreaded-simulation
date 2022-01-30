3682ba8356099b144100b32e57908b522840fb2a
Andrzej Perczak <linux@andrzejperczak.com>
2022-01-30 19:35:30 +0100

    Add Helpline class
    Used to obtain Operator instance to /make an emergency call/.

---
+++ src/main/java/simulation/simulation/Helpline.java
@@ -0,0 +1,28 @@
+package simulation.simulation;
+
+import java.util.ArrayList;
+
+public class Helpline {
+    private static final int OPERATORS_COUNT = 10;
+    public static final Helpline INSTANCE = new Helpline();
+
+    ArrayList<Operator> operatorsList;
+    int currentOperator = 0;
+
+    private Helpline() {
+        operatorsList = new ArrayList<Operator>();
+        for(int i = 0; i < OPERATORS_COUNT; i++) {
+            operatorsList.add(new Operator(i));
+        }
+    }
+
+    public static Helpline getInstance() {
+        return INSTANCE;
+    }
+
+    public synchronized Operator getOperator() {
+        if (currentOperator == OPERATORS_COUNT)
+            currentOperator = 0;
+        return operatorsList.get(currentOperator++);
+    }
+}
