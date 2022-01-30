6481265c2dcde2fc9ab0bcda786ee4d523a10866
Administrator <belhub24@gmail.com>
2022-01-30 21:54:22 +0100

    Adding Timer

--- src/main/java/simulation/simulation/AccidentGenerator.java
+++ src/main/java/simulation/simulation/AccidentGenerator.java
@@ -1,6 +1,8 @@
 package simulation.simulation;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import simulation.gui.SimulationFrame;
 
@@ -8,34 +10,22 @@ public class AccidentGenerator {
     private static final int THREAD_SLEEP_MIN = 5000;
     private static final int THREAD_SLEEP_MAX = 10000;
     private static final Random rand = new Random();
-    boolean threadShouldStop = false;
-    Thread accidentThread;
+    private final Timer timer = new Timer();
 
-    final Runnable accidentRunnable = new Runnable() {
+    private class Task extends TimerTask {
         @Override
         public void run() {
-            accidentAction();
-        }
-    };
-
-    void accidentAction() {
-        try {
-            Thread.sleep(THREAD_SLEEP_MIN);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        while (!threadShouldStop) {
             SimulationFrame.accidends.add(new Accident(AccidentType.getRandomAccident(AccidentType.class)));
             int sleepTime = rand.nextInt(THREAD_SLEEP_MAX - THREAD_SLEEP_MIN + 1) + THREAD_SLEEP_MIN;
-            try {
-                Thread.sleep(sleepTime);
-            } catch (InterruptedException e) {
-            }
+            timer.schedule(new Task(), sleepTime);
         }
     }
 
     public AccidentGenerator() {
-        accidentThread = new Thread(accidentRunnable);
-        accidentThread.start();
+        timer.schedule(new Task(), THREAD_SLEEP_MIN);
+    }
+
+    public void stopGenerator() {
+        timer.cancel();
     }
 }
